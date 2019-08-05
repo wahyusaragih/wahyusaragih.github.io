3ac2aacb5e1879e63f1a9c18b604b5b8a405b859
Wahyu Saragih <wahyusaragih@yahoo.com>
2019-08-05 19:43:29 +0700

    initial commit

---
+++ assets/js/anim.js
@@ -0,0 +1,44 @@
+$(document).on("click", "a", function (e) {
+  e.preventDefault();
+  var id = $(this).attr("href"),
+    topSpace = 30;
+  //alert(id);
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - topSpace
+  }, 1000);
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+  showDivs2(slideIndex += n);
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
